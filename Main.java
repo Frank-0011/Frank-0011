eec28bc54b008b6d1df210f848ad4ec790892f04
Frank-0011 <frank69858569@gmail.com>
2024-07-14 19:35:33 +0330

    Create Main.java
    first one

---
+++ Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+        System.out.println("this is my first java programing");
+
+        int x = 12;
+        int y = 73498;
+        int z = x * y;
+        System.out.println(z);
+        System.out.println("if you see the number it works!");
+        System.out.println("and also doubles");
+
+        double a = 20.323;
+        double b = 987.79;
+        double c = a= b;
+        double d = a * b;
+        System.out.println(c);
+        System.out.println(d);
+
+        /*
+        and this is how to comment!
+         */
+        /*long g = 19L:*/
+
+        float f = 3.14444444444444444f;
+        System.out.println(f);
+
+
+
+    }
+}
